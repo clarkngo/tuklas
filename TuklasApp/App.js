8ec57c82e1cc1c7473c528e7596d558aa42149cd
Clark Ngo <30854108+clarkngo@users.noreply.github.com>
2024-11-02 21:26:43 +0000

    #1 mvp

---
+++ TuklasApp/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import DiscoverScreen from './screens/DiscoverScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Discover" component={DiscoverScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
